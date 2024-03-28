e5980109596c60fc7f352ffe1054eee17e2831d6
Spook <spook.devv@gmail.com>
2024-03-28 09:40:30 -0400

    Initial Commit

---
+++ screens/ManageExpense.js
@@ -0,0 +1,93 @@
+import { useContext, useLayoutEffect } from "react";
+import { StyleSheet, View } from "react-native";
+import IconButton from "../components/ui/IconButton";
+import { GlobalStyles } from "../constants/styles";
+import Button from "../components/ui/Button";
+import { ExpensesContext } from "../store/expenses-context";
+
+function ManageExpense({ route, navigation }) {
+  const expensesCtx = useContext(ExpensesContext);
+  const editedExpenseId = route.params?.expenseId;
+  const isEditing = !!editedExpenseId; // The !!someVariable expression is a common JavaScript pattern used to convert a value to its corresponding boolean representation
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: isEditing ? "Edit Expense" : "Add Expense",
+    });
+  }, [navigation, isEditing]);
+
+  function deleteExpense() {
+    expensesCtx.deleteExpense(editedExpenseId);
+    navigation.goBack();
+  }
+
+  function cancelHandler() {
+    navigation.goBack();
+  }
+
+  function confirmHandler() {
+    if (isEditing) {
+      expensesCtx.updateExpense(editedExpenseId, {
+        description: "Test update",
+        amount: 29.99,
+        date: new Date("2024-03-23"),
+      });
+    } else {
+      expensesCtx.addExpense({
+        description: "Test add",
+        amount: 19.99,
+        date: new Date("2024-03-22"),
+      });
+    }
+    navigation.goBack();
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.buttonContainer}>
+        <Button style={styles.button} mode="flat" onPress={cancelHandler}>
+          Cancel
+        </Button>
+        <Button style={styles.button} onPress={confirmHandler}>
+          {isEditing ? "Update" : "Add"}
+        </Button>
+      </View>
+      {isEditing && (
+        <View style={styles.deleteContainer}>
+          <IconButton
+            icon="trash"
+            color={GlobalStyles.colors.error500}
+            size={36}
+            onPress={deleteExpense}
+          />
+        </View>
+      )}
+    </View>
+  );
+}
+
+export default ManageExpense;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary800,
+  },
+  deleteContainer: {
+    marginTop: 16,
+    paddingTop: 8,
+    borderTopWidth: 2,
+    borderTopColor: GlobalStyles.colors.primary200,
+    alignItems: "center",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8,
+  },
+});
