11504f4ae47d63dfdf9f75e6a1946ba577bb52e1
Spook <spook.devv@gmail.com>
2024-03-28 09:59:42 -0400

    Configuring the form input elements

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -4,6 +4,7 @@ import IconButton from "../components/ui/IconButton";
 import { GlobalStyles } from "../constants/styles";
 import Button from "../components/ui/Button";
 import { ExpensesContext } from "../store/expenses-context";
+import ExpenseForm from "../components/ManageExpense/ExpenseForm";
 
 function ManageExpense({ route, navigation }) {
   const expensesCtx = useContext(ExpensesContext);
@@ -44,6 +45,7 @@ function ManageExpense({ route, navigation }) {
 
   return (
     <View style={styles.container}>
+      <ExpenseForm />
       <View style={styles.buttonContainer}>
         <Button style={styles.button} mode="flat" onPress={cancelHandler}>
           Cancel
